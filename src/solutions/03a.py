33aa3f0875c15c08bf05e32f8372c02d7e747a01
Luke Sawczak <luke@sawczak.com>
2023-12-03 09:15:11 -0500

    03a

--- src/solutions/03a.py
+++ src/solutions/03a.py
@@ -24,8 +24,15 @@ RE_DIGITS = r'(\d+)'
 def is_symbol(s: str) -> bool:
     return s in '!@#$%^&*-=_+[]{};:,<>/?\\|\'\"~`'
 
-def is_adjacent_to_symbol(row: int, start: int, end: int) -> bool:
-    return True
+def is_adjacent_to_symbol(i_row: int, start: int, end: int) -> bool:
+    for i_g_row in range(max(0, i_row - 1), min(len(grid), i_row + 2)):
+        g_row = grid[i_g_row]
+        adj_start = max(0, start - 1)
+        adj_end = min(len(g_row), end + 1)
+        adj_space = g_row[adj_start:adj_end]
+        if any(is_symbol(c) for c in adj_space):
+            return True
+    return False
 
 # Logic
 
@@ -34,12 +41,12 @@ grid = []
 
 with open(f'src/inputs/{N:0>2}.txt', 'r') as f:
     grid = [line for line in stripped_lines(f)]
-    for (row, line) in enumerate(grid):
+    for (i_row, line) in enumerate(grid):
         for m in re.finditer(RE_DIGITS, line):
             start, end = m.start(), m.end()
             n = int(m.group())
 
-            if is_adjacent_to_symbol(row, start, end):
+            if is_adjacent_to_symbol(i_row, start, end):
                 result += n
 
 with open(f'src/outputs/{N:0>2}{S}.txt', 'w') as f:
