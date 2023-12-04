fb9e1af7c81ee32063db69b786ba7c1e72989d31
Luke Sawczak <luke@sawczak.com>
2023-12-04 07:26:45 -0500

    cleaner

--- src/solutions/04b.py
+++ src/solutions/04b.py
@@ -19,40 +19,10 @@ def stripped_lines(f) -> Iterable:
 
 RE_CARD = r'Card +(\d+)'
 
-# Helpers
-
-
-
 # Logic
 
 result = 0
 copies = {}
-
-# with open(f'src/inputs/{N:0>2}.txt', 'r') as f:
-#     for line in stripped_lines(f):
-#         card, line = line.split(':')
-#         this_card_n = int(re.findall(RE_CARD, card)[0])
-
-#         if this_card_n not in copies:
-#             copies[this_card_n] = 1
-
-#         for _ in range(copies[this_card_n]):
-#             winning, having = line.split('|')
-#             winning = set(int(n) for n in winning.strip().split())
-#             having = set(int(n) for n in having.strip().split())
-
-#             both = having.intersection(winning)
-            
-#             # Add new cards
-#             for i in range(len(both)):
-#                 next_card_n = this_card_n + i + 1
-#                 copies[next_card_n] = copies.get(next_card_n, 1) + 1
-            
-#             # Tally number of cards done
-#             result += 1
-
-# with open(f'src/outputs/{N:0>2}{S}.txt', 'w') as f:
-#     f.write(f'{result}')
 
 with open(f'src/inputs/{N:0>2}.txt', 'r') as f:
     for line in stripped_lines(f):
