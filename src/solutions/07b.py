ac44db4520f9758a509662e034e00df251d5d1de
Mr <sawczak@tdchristian.ca>
2023-12-07 17:11:53 -0500

    7b before alternative approach

--- src/solutions/07b.py
+++ src/solutions/07b.py
@@ -33,7 +33,7 @@ class Hand:
     """
     Note that Hand sorting will place better hands greater, so at the end.
     """
-    STRENGTHS: str = 'AKQJT98765432'[::-1]
+    STRENGTHS: str = 'AKQT98765432J'[::-1]
     cards: str
     bid: int
 
@@ -41,48 +41,92 @@ class Hand:
         self.cards = cards
         self.bid = bid
 
-    def is_5oak(self: Hand) -> bool:
-        return len(set(self.cards)) == 1
+    @staticmethod
+    def is_5oak(cards: str) -> bool:
+        return len(set(cards)) == 1
 
-    def is_4oak(self: Hand) -> bool:
-        s = set(self.cards)
-        return any(self.cards.count(c) == 4 for c in s)
+    @staticmethod
+    def is_4oak(cards: str) -> bool:
+        s = set(cards)
+        return any(cards.count(c) == 4 for c in s)
 
-    def is_full(self: Hand) -> bool:
-        s = set(self.cards)
-        return (len(s) == 2) and any(self.cards.count(c) == 3 for c in s)
+    @staticmethod
+    def is_full(cards: str) -> bool:
+        s = set(cards)
+        return (len(s) == 2) and any(cards.count(c) == 3 for c in s)
 
-    def is_3oak(self: Hand) -> bool:
-        s = set(self.cards)
-        return (len(s) == 3) and any(self.cards.count(c) == 3 for c in s)
+    @staticmethod
+    def is_3oak(cards: str) -> bool:
+        s = set(cards)
+        return (len(s) == 3) and any(cards.count(c) == 3 for c in s)
 
-    def is_2pair(self: Hand) -> bool:
-        s = set(self.cards)
-        return (len(s) == 3) and (sum(self.cards.count(c) == 2 for c in s) == 2)
+    @staticmethod
+    def is_2pair(cards: str) -> bool:
+        s = set(cards)
+        return (len(s) == 3) and (sum(cards.count(c) == 2 for c in s) == 2)
 
-    def is_1pair(self: Hand) -> bool:
-        s = set(self.cards)
-        return (len(s) == 4) and (sum(self.cards.count(c) > 1 for c in s) == 1)
+    @staticmethod
+    def is_1pair(cards: str) -> bool:
+        s = set(cards)
+        return (len(s) == 4) and (sum(cards.count(c) > 1 for c in s) == 1)
 
-    def is_high(self: Hand) -> bool:
-        return len(set(self.cards)) == 5
+    @staticmethod
+    def is_high(cards: str) -> bool:
+        return len(set(cards)) == 5
     
-    def get_type_rank(self: Hand) -> list[int]:
-        checkers = [
-            Hand.is_5oak,
-            Hand.is_4oak,
-            Hand.is_full,
-            Hand.is_3oak,
-            Hand.is_2pair,
-            Hand.is_1pair,
-            Hand.is_high
-        ]
-        return [int(c(self)) for c in checkers]
+    @staticmethod
+    def get_joker_variants(cards: str) -> Iterable[str]:
+        nonjokers = ''.join(c for c in cards if (c != 'J'))
+        jokers = ''.join(c for c in cards if (c == 'J'))
+
+        def _mutate(_cards: str) -> str:
+            for s in Hand.STRENGTHS[1:]:    # not J
+                if _cards[1:]:
+                    for _rest in _mutate(_cards[1:]):
+                        yield s + _rest
+                else:
+                    yield s
+        
+        for variant in _mutate(jokers):
+            yield variant + nonjokers
+
+    @staticmethod
+    def jokerize(variants: list[str], checker: callable) -> bool:
+        return any(checker(v) for v in variants)
     
+    def get_type_rank(self: Hand) -> int:
+        n_Js = self.cards.count('J')
+
+        # short-circuiting...
+        # 4 or 5 Js means 5 of a kind
+        if n_Js > 3:
+            return 7
+        
+        # 3 Js means 
+
+        variants = list(Hand.get_joker_variants(self.cards))
+
+        if Hand.jokerize(variants, Hand.is_5oak):
+            return 7
+        elif Hand.jokerize(variants, Hand.is_5oak):
+            return 6
+        elif Hand.jokerize(variants, Hand.is_4oak):
+            return 5
+        elif Hand.jokerize(variants, Hand.is_3oak):
+            return 4
+        elif Hand.jokerize(variants, Hand.is_2pair):
+            return 3
+        elif Hand.jokerize(variants, Hand.is_1pair):
+            return 2
+        elif Hand.jokerize(variants, Hand.is_high):
+            return 1
+        else:
+            return 0
+
     def get_card_rank(self: Hand) -> list[int]:
         return [Hand.STRENGTHS.index(c) for c in self.cards]
     
-    def get_cmp_factors (self: Hand) -> tuple[list[int]]:
+    def get_cmp_factors (self: Hand) -> tuple[int, list[int]]:
         return (self.get_type_rank(), self.get_card_rank())
     
     def __eq__(self: Hand, other: object) -> bool:
@@ -113,6 +157,9 @@ with open(f'src/{INPUTS}/{N:0>2}.txt', 'r') as f:
     hands.sort()
     
     for (i, hand) in enumerate(hands):
+        # if hand.cards.count('J') == 2:
+        #     print(hand.get_type_rank())
+
         result += (i + 1) * hand.bid
 
 with open(f'src/{OUTPUTS}/{N:0>2}{S}.txt', 'w') as f:
